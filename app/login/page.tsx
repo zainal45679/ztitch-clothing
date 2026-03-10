d8e2fa4d4a5c9fc7034a4c6d75b4ed246ee09c49
Zain Al Faisal <zainaldemo@gmail.com>
2026-03-10 12:19:17 +0530

    Login and Sign up re-designed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,21 +4,35 @@ import React, { useState } from 'react'
 import Password from '../_components/password'
 import Eye from '@/svg/Eye'
 import Link from 'next/link'
+import z from "zod";
+import { ZodAny } from 'zod'
+import { Form, useForm } from 'react-hook-form'
+
+const loginSchema = z.object({
+    username: z.string().min(1, "Username is Required"),
+    password: z.string().min(8, "Minimum 8 characters required")
+})
 
 const page = () => {
 
+const { register } = useForm()
+
 const [pass, setPass] = useState("password")
+
+const submit = () =>{
+
+}
 
   return (
     <div className='flex items-center justify-center h-[90vh] bg-[#e5dccd]'>
-        <div className='flex flex-col  gap-4 bg-[#24180c]/70 p-10 w-100'>
+        <form className='flex flex-col  gap-4 bg-[#24180c]/70 p-10 w-100'>
             <div className='flex flex-col items-center'>
                 <h1 className='text-4xl'>LOG IN </h1>
                 <h1 className='text-xl'>Sign in to get started</h1>
             </div>
             <div>
                 <h1 className='text-2xl'>Email</h1>
-                <input type="text" placeholder='Choose a username' className='border border-[#e5dccd]/30 w-full p-2 text-xl' />
+                <input {...register} type="text" placeholder='Choose a username' className='border border-[#e5dccd]/30 w-full p-2 text-xl' />
             </div>
             <div className='relative'>
                 <Password type={pass} title='Password' place='Enter a password' className1='text-2xl' className2='border border-[#e5dccd]/30 w-full p-2 text-xl'/>
@@ -30,7 +44,7 @@ const [pass, setPass] = useState("password")
             <div>
                 <h1 className='text-xl'> Don't have an account ? <Link href="/signup" className='text-blue-400'> SignUp Here</Link> </h1>
             </div>
-        </div>
+        </form>
     </div>
   )
 }
--- app/shop/[products]/page.tsx
+++ app/shop/[products]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import img1 from "../../../public/_images/IMG_9027.jpg";
@@ -41,25 +41,22 @@ const products = [
   },
 ];
 
-
 const page = () => {
+  const [inputValue, setInputValue] = useState("");
 
-const [inputValue, setInputValue] = useState("")
+  const [handleSort, setHandleSort] = useState(true);
 
-const [handleSort, setHandleSort] = useState(true)
+  let order = "asc";
 
-let order = "asc";
+  const filteredProduct = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue.toLowerCase());
+  });
 
-const filteredProduct = products.filter((product) => {
-  return product.title.toLowerCase().includes(inputValue.toLowerCase());
-});
-
-const sortedArray = ()=>{ [...filteredProduct].sort((a,b)=> {
- return parseInt(a.price) - parseInt(b.price)
-})
-}
-
-
+  const sortedArray = 
+    [...filteredProduct].sort((a, b) => {
+      return parseInt(a.price) - parseInt(b.price);
+    });
+  
 
   return (
     <div className="bg-[#e5dccd] ">
@@ -70,28 +67,29 @@ const sortedArray = ()=>{ [...filteredProduct].sort((a,b)=> {
             placeholder="Search"
             className="bg-transparent rounded px-2 py-1 text-2xl outline-none"
             value={inputValue}
-            onChange={(e)=>setInputValue(e.target.value)}
+            onChange={(e) => setInputValue(e.target.value)}
           />
           <h1 className="text-3xl">5 PRODUCTS</h1>
-          <button onClick={()=> {setHandleSort(!handleSort) 
-            
-           } } className="text-white bg-[#24180c]/70 px-3 py-1 rounded text-2xl">
+          <button
+            onClick={() => {
+              setHandleSort(!handleSort);
+            }}
+            className="text-white bg-[#24180c]/70 px-3 py-1 rounded text-2xl"
+          >
             SORT BY PRICE
           </button>
         </div>
       </div>
       <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 md:grid-rows-2 md:gap-4 gap-2 md:pt-7 pt-2 px-4">
-        {
-            (handleSort ? filteredProduct : sortedArray).map((items, i) => (
-            <Card
-                key={i}
-                image={items.image}
-                title={items.title}
-                caption={items.caption}
-                price={items.price}
-            />
-            ))
-        }
+        {(handleSort ? filteredProduct : sortedArray).map((items, i) => (
+          <Card
+            key={i}
+            image={items.image}
+            title={items.title}
+            caption={items.caption}
+            price={items.price}
+          />
+        ))}
       </div>
     </div>
   );
