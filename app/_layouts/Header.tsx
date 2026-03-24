76f7ee94dfa18dfd2b50e573c2d383acc5d6f89d
Zain Al Faisal <zainaldemo@gmail.com>
2026-03-24 22:40:35 +0530

    Hamburger animation added

--- app/_layouts/Header.tsx
+++ app/_layouts/Header.tsx
@@ -1,51 +1,64 @@
 "use client";
-import Link from 'next/link'
-import React, { useState } from 'react'
-import Cart from '../../svg/Cart'
-import Hamburger from '@/svg/Hamburger'
-import Close from '@/svg/Close';
-import { useCart } from 'react-use-cart';
+import Link from "next/link";
+import React, { useState } from "react";
+import Cart from "../../svg/Cart";
+import Hamburger from "@/svg/Hamburger";
+import Close from "@/svg/Close";
+import { useCart } from "react-use-cart";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
 
-const [menu, setMenu] = useState(false);
+  const { totalUniqueItems } = useCart();
 
-const { totalUniqueItems } = useCart()
-
-    return (
-    <div className='flex bg-[#24180c] md:h-15 items-center justify-between p-3 px-5  md:p-10 z-50'>
-        <h2 className='text-gray-200 text-[25px] md:text-5xl p-0 '>Z'TITCH</h2>
-        <div className='hidden md:flex  gap-8 text-gray-200 text-2xl'>
-            <Link href="/">Home</Link>
-            <Link href="/shop">Shop</Link>
-            <Link href="">About</Link>
-            <Link href="">Blog</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="">Page</Link>
+  return (
+    <div className="flex bg-[#24180c] md:h-15 items-center justify-between p-3 px-5  md:p-10 z-50">
+      <Link href="/" className="text-gray-200 text-[25px] md:text-5xl p-0 ">Z'TITCH</Link>
+      <div className="hidden md:flex  gap-8 text-gray-200 text-2xl">
+        <Link href="/">Home</Link>
+        <Link href="/shop">Shop</Link>
+        <Link href="">About</Link>
+        <Link href="">Blog</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="">Page</Link>
+      </div>
+      <div className="flex gap-7">
+        <div className="flex relative cursor-pointer mt-2">
+          <Link href="/cart">
+            <Cart className="md:w-6 w-5.5 md:h-6 h-5.5 text-gray-200 " />
+          </Link>
+          <span className="absolute md:-top-5 md:-right-5 -top-3.5 -right-5 bg-gray-200/70 pb-0.5 text-gray-800 md:px-2 px-[7.5] max-md:text-sm rounded-full font-bold">
+            {totalUniqueItems}
+          </span>
         </div>
-        <div className='flex gap-7'>
-            <div className='flex relative cursor-pointer mt-2'>
-                <Link href="/cart"><Cart className='md:w-6 w-5.5 md:h-6 h-5.5 text-gray-200 ' /></Link>
-                <span className='absolute md:-top-5 md:-right-5 -top-3.5 -right-5 bg-gray-200/70 pb-0.5 text-gray-800 md:px-2 px-[7.5] max-md:text-sm rounded-full font-bold'>{totalUniqueItems}</span>
-            </div>
-            <Hamburger onClick={() => setMenu(true)} className='h-8 w-8 md:hidden text-gray-200'/>
-            { menu === true && 
-            (<div className='bg-[#24180c]/95 fixed h-screen max-md:w-1/3 max-sm:w-1/2 z-50 right-0 top-0'>
-                <div><Close onClick={()=>setMenu(false)} className='h-8 m-2 absolute top-1 right-2 text-[#e5dccd]/50'/></div>
-                <div className='flex flex-col text-[#e5dccd]/90 px-7 py-15 text-2xl gap-2'>
-                    <Link href="/">Home</Link>
-                    <Link href="/shop">Shop</Link>
-                    <Link href="">About</Link>
-                    <Link href="">Blog</Link>
-                    <Link href="/contact">Contact</Link>
-                    <Link href="">Page</Link>
-                </div>
-            </div>)}
+        <Hamburger
+          onClick={() => setMenu(true)}
+          className="h-8 w-8 md:hidden text-gray-200"
+        />
+        <div
+          className={`bg-[#24180c]/95 fixed h-screen max-md:w-1/3 max-sm:w-1/2 z-50 top-0 right-0 
+            transform transition-transform duration-300 ease-in-out
+            ${menu ? "translate-x-0" : "translate-x-full"}`}
+        >
+          <div>
+            <Close
+              onClick={() => setMenu(false)}
+              className="h-8 m-2 absolute top-1 right-2 text-[#e5dccd]/50"
+            />
+          </div>
+
+          <div className="flex flex-col text-[#e5dccd]/90 px-7 py-15 text-2xl gap-2">
+            <Link onClick={()=>setMenu(false)} href="/">Home</Link>
+            <Link onClick={()=>setMenu(false)} href="/shop">Shop</Link>
+            <Link onClick={()=>setMenu(false)} href="">About</Link>
+            <Link onClick={()=>setMenu(false)} href="">Blog</Link>
+            <Link onClick={()=>setMenu(false)} href="/contact">Contact</Link>
+            <Link onClick={()=>setMenu(false)} href="">Page</Link>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
-
-
+export default Header;
